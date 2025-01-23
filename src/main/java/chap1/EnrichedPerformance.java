df20736e603389b25f802ebd64cd85ba5db06369
onezerokang <onezerokang@gmail.com>
2025-01-23 19:27:38 +0900

    chap1 EnrichedPerformance에 amount 데이터 추가

--- src/main/java/chap1/EnrichedPerformance.java
+++ src/main/java/chap1/EnrichedPerformance.java
@@ -4,11 +4,13 @@ public class EnrichedPerformance {
     private final String playId;
     private final int audience;
     private final Play play;
+    private final int amount;
 
-    public EnrichedPerformance(final String playId, final int audience, final Play play) {
+    public EnrichedPerformance(final String playId, final int audience, final Play play, final int amount) {
         this.playId = playId;
         this.audience = audience;
         this.play = play;
+        this.amount = amount;
     }
 
     public String getPlayId() {
--- src/main/java/chap1/StatementData.java
+++ src/main/java/chap1/StatementData.java
@@ -16,12 +16,34 @@ public class StatementData {
 
     private List<EnrichedPerformance> enrichPerformance(final List<Performance> performances) {
         return performances.stream()
-                .map(p -> new EnrichedPerformance(p.playId(), p.audience(), playFor(p)))
+                .map(p -> new EnrichedPerformance(p.playId(), p.audience(), playFor(p), amountFor(p)))
                 .toList();
     }
 
     private Play playFor(final Performance perf) {
         return plays.getPlay(perf.playId());
+    }
+
+    private int amountFor(final Performance aPerformance) {
+        int result = 0;
+        switch (playFor(aPerformance).type()) {
+            case "tragedy": // 비극
+                result = 40000;
+                if (aPerformance.audience() > 30) {
+                    result += 1000 * (aPerformance.audience() - 30);
+                }
+                break;
+            case "comedy": // 희극
+                result = 30000;
+                if (aPerformance.audience() > 20) {
+                    result += 10000 + 500 * (aPerformance.audience() - 20);
+                }
+                result += 300 * aPerformance.audience();
+                break;
+            default:
+                throw new IllegalStateException("알 수 없는 장르: %s".formatted(playFor(aPerformance).type()));
+        }
+        return result;
     }
 
     public String getCustomer() {
